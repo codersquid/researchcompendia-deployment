902581e4354f806c5971c37d2327305701ece3ee
codersquid <github@codersquid.com>
2014-02-05 22:58:21 -0600

    1. adds tasks per environment. 2. explictely specify encoding for new database. 3. lots of other crap such as template tweaking

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,21 @@
+"""Deployment scripts for ResearchCompendia
+with credit to Scipy-2014 fabric.py
+
+fab <dev|staging|prod|vagrant> <deploy|provision>[:<git ref>]
+
+deploy: deploys the site to the specified environment. If no git ref is provided, deploys HEAD
+provision: provisions a box to run the site. is not idempotent. do not rerun.
+git ref: a git branch, hash, tag
+
+
+Example usages:
+$ fab staging deploy
+$ fab prod deploy:1.0.1-b7
+
+"""
 import datetime, string, random
 from os.path import join, dirname, abspath
+import fabric.api
 from fabric.api import run, task, env, cd, sudo, local, put
 from fabric.contrib.files import sed
 from fabtools import require, supervisor, postgres, deb, files
@@ -8,37 +24,91 @@ from fabtools.user import home_directory
 import fabtools
 
 env.disable_known_hosts = True
-#env.user = 'vagrant'
-env.hosts = [
-    #'researchcompendia.org',
-    #'labs.researchcompendia.org',
-    # my remote dev box
-    '67.207.156.211',
-    #'vagrant@127.0.0.1:2222',
-]
-if env.user == 'vagrant':
-    env.key_filename = local('vagrant ssh-config | grep IdentityFile | cut -f4 -d " "', capture=True)
+
 SITE_USER = 'tyler'
+SITE_GROUP = 'tyler'
 SITE_NAME = 'tyler'
 SITE_REPO = 'git://github.com/researchcompendia/researchcompendia.git'
-SITE_ENVIRONMENT = 'vagrant'
 FAB_HOME = dirname(abspath(__file__))
+TEMPLATE_DIR = join(FAB_HOME, 'templates')
+
+@task
+def dev():
+    env.update({
+        'carbon': '10.176.162.45',
+        'site': 'dev.codersquid.com',
+        'available': 'researchcompendia',
+        'hosts': ['dev.codersquid.com'],
+        'site_environment': 'dev',
+    })
+
+
+@task
+def staging():
+    env.update({
+        'carbon': '10.176.162.45',
+        'site': 'labs.researchcompendia.org',
+        'available': 'researchcompendia',
+        'hosts': ['labs.researchcompendia.org'],
+        'site_environment': 'staging',
+    })
+
+
+@task
+def prod():
+    env.update({
+        'carbon': '10.176.162.45',
+        'site': 'researchcompendia.org',
+        'available': 'researchcompendia',
+        'hosts': ['researchcompendia.org'],
+        'site_environment': 'prod',
+    })
+
+
+@task
+def vagrant():
+    env.update({
+        'carbon': '10.176.162.45',
+        'user': 'vagrant',
+        'site': '127.0.0.1:2222',
+        'available': 'researchcompendia',
+        'hosts': ['127.0.0.1:2222'],
+        'site_environment': 'vagrant',
+        'key_filename': local('vagrant ssh-config | grep IdentityFile | cut -f4 -d " "', capture=True),
+    })
+
+
+@task
+def uname():
+    fabric.api.require('site', 'available', 'hosts', 'site_environment',
+        provided_by=('dev', 'staging', 'prod', 'vagrant'))
+    run('uname -a')
 
 
 @task
 def deploy(version_tag=None):
+    fabric.api.require('site', 'available', 'hosts', 'site_environment',
+        provided_by=('dev', 'staging', 'prod', 'vagrant'))
+
     supervisor.stop_process(SITE_NAME)
+
     new_env = virtualenv_name(commit=version_tag)
     mkvirtualenv(new_env)
     update_site_version(new_env)
     update_repo(commit=version_tag)
     install_site_requirements(new_env)
     collectstatic()
+
     supervisor.start_process(SITE_NAME)
 
 
 @task
-def setup(version_tag=None):
+def provision(version_tag=None):
+    """Run only once to provision a new host.
+    This is not idempotent. Only run once!
+    """
+    fabric.api.require('site', 'available', 'hosts', 'site_environment',
+        provided_by=('dev', 'staging', 'prod', 'vagrant'))
     install_dependencies()
     lockdowns()
     setup_database()
@@ -48,10 +118,7 @@ def setup(version_tag=None):
     setup_nginx()
     setup_supervisor()
     setup_rabbitmq()
-
-    ## collectd
-    # install, files, restart
-    #setup_collectd()
+    setup_collectd()
 
     # statsite
     #setup_statsite()
@@ -68,6 +135,13 @@ def setup(version_tag=None):
     # restart supervisor
 
 
+def setup_collectd():
+    hostname = run('hostname')
+    upload_template('collectd.conf', '/etc/collectd/collectd.conf', use_jinja=True,
+        context={'carbonhost': env.carbon, 'hostname': hostname},
+        template_dir=TEMPLATE_DIR, use_sudo=True, chown=True, user=SITE_USER)
+    sudo('/etc/init.d/collectd restart')
+
 def setup_rabbitmq(user=None):
     if user is None:
         user = SITE_USER
@@ -75,23 +149,48 @@ def setup_rabbitmq(user=None):
     sudo('rabbitmqctl delete_user guest')
     sudo('rabbitmqctl add_user %s "%s"' % (user, secret))
     sudo('rabbitmqctl set_permissions -p / %s ".*" ".*" ".*"' % user)
-    template_dir = join(FAB_HOME, 'templates')
     destination_file = join(home_directory(user), 'site', 'env', 'DJANGO_BROKER_URL')
     upload_template('DJANGO_BROKER_URL', destination_file, use_jinja=True, context={'password': secret},
-        template_dir=template_dir, use_sudo=True, chown=True, user=SITE_USER)
+        template_dir=TEMPLATE_DIR, use_sudo=True, chown=True, user=SITE_USER)
 
 
 def setup_nginx():
-    template_dir = join(FAB_HOME, 'templates')
-    upload_template('researchcompendia_nginx', '/etc/nginx/sites-available/researchcompendia',
-            use_jinja=True, use_sudo=True, template_dir=template_dir)
-    # figure out how to do the status conf
+    site_root = join(home_directory(SITE_USER), 'site')
+    upload_template('researchcompendia_nginx',
+        '/etc/nginx/sites-available/researchcompendia',
+        context={
+            'server_name': env.site,
+            'access_log': join(site_root, 'logs', 'access.log'),
+            'error_log': join(site_root, 'logs', 'error.log'),
+            'static_location': join(site_root, 'static'),
+            'media_location': join(site_root, 'media'),
+        },
+        use_jinja=True, use_sudo=True, template_dir=TEMPLATE_DIR)
     require.nginx.enabled('researchcompendia')
 
 
 def setup_supervisor():
-    put(template_path('researchcompendia_web.conf'), '/etc/supervisor/conf.d/researchcompendia_web.conf', use_sudo=True)
-    put(template_path('celeryd.conf'), '/etc/supervisor/conf.d/celeryd.conf', use_sudo=True)
+    site_root = join(home_directory(SITE_USER), 'site')
+    upload_template('researchcompendia.conf', 
+        '/etc/supervisor/conf.d/researchcompendia_web.conf',
+        context={
+            'command': 'envdir %s/ %s' % (join(site_root, 'bin', 'runnerenv'), join(site_root, 'bin', 'runserver.sh')),
+            'user': SITE_USER,
+            'group': SITE_GROUP,
+            'logfile': join(site_root, 'logs', 'gunicorn_supervisor.log'),
+
+        },
+        use_jinja=True, use_sudo=True, template_dir=TEMPLATE_DIR)
+    upload_template('celeryd.conf', 
+        '/etc/supervisor/conf.d/celeryd.conf',
+        context={
+            'command': 'envdir %s/ %s' % (join(site_root, 'bin', 'runnerenv'), join(site_root, 'bin', 'celeryworker.sh')),
+            'user': SITE_USER,
+            'group': SITE_GROUP,
+            'logfile': join(site_root, 'logs', 'celery_worker.log'),
+
+        },
+        use_jinja=True, use_sudo=True, template_dir=TEMPLATE_DIR)
     supervisor.update_config()
 
 
@@ -115,8 +214,8 @@ def migrate_and_load_database():
     envdir = join(home_directory(SITE_USER), 'site', 'env')
     djangodir = join(home_directory(SITE_USER), 'site', SITE_NAME, 'companionpages')
     with cd(djangodir):
-        vsu('envdir %s ./manage.py syncdb --noinput --migrate' % envdir)
-        vsu('envdir %s ./manage.py loaddata fixtures/*' % envdir)
+        vsu('envdir %s/ ./manage.py syncdb --noinput --migrate' % envdir)
+        vsu('envdir %s/ ./manage.py loaddata fixtures/*' % envdir)
 
 
 def install_site_requirements(virtualenv):
@@ -131,20 +230,16 @@ def setup_database():
     if not postgres.user_exists(SITE_USER):
         su('createuser -S -D -R -w %s' % SITE_USER, 'postgres')
     if not postgres.database_exists(SITE_USER):
-        su('createdb -w -O %s %s' % (SITE_USER, SITE_USER), 'postgres')
+        require.postgres.database(SITE_USER, SITE_USER, encoding='UTF8', locale='en_US.UTF-8')
 
 def setup_user():
     if not fabtools.user.exists(SITE_USER):
         sudo('useradd -s/bin/bash -d/home/%s -m %s' % (SITE_USER, SITE_USER))
 
-    bash_aliases = join(home_directory(SITE_USER), '.bash_aliases')
-    put(template_path('_bash_aliases'), bash_aliases, use_sudo=True)
-    sudo('chown %s:%s %s' % (SITE_USER, SITE_USER, bash_aliases))
-
     site_root = join(home_directory(SITE_USER), 'site')
     bindir = join(site_root, 'bin')
     envdir = join(site_root, 'env')
-    localenvdir = join(FAB_HOME, 'env', SITE_ENVIRONMENT, '*')
+    localenvdir = join(FAB_HOME, 'env', env.site_environment, '*')
 
     with cd(home_directory(SITE_USER)):
         su('mkdir -p venvs site')
@@ -202,6 +297,7 @@ def install_debian_packages():
         'libmemcached-dev',
         'ack-grep',
         'rabbitmq-server',
+        'collectd',
     ])
 
 
@@ -261,7 +357,7 @@ def collectstatic():
     # this shouldn't be necessary but is a consequence of using django-storages with the boto backend
     # and as of now, django-storages doesn't support separate containers for static and media.
     with cd(djangodir):
-        vsu('envdir %s ./manage.py collectstatic --ignore *logs* --ignore *materials* --ignore *articles*' % envdir)
+        vsu('envdir %s/ ./manage.py collectstatic --ignore *logs* --ignore *materials* --ignore *articles*' % envdir)
 
 
 def get_site_version():
@@ -286,4 +382,5 @@ def template_path(filename):
 
 
 def mkvirtualenv(virtualenv):
-    su('source ~/.bash_aliases; mkvirtualenv %s' % virtualenv)
+    with cd(join(home_directory(SITE_USER), 'venvs')):
+        su('virtualenv %s' % virtualenv)
